0148f3543697847a1fb0639186f87b8c5e63d68a
Rakesh Ranjan <rakeshranjan.568@gmail.com>
2017-10-05 21:22:32 +0530

    Consistent and incremental snapshotting.

--- aviatrix_ha.py
+++ aviatrix_ha.py
@@ -22,6 +22,10 @@ def lambda_handler(event, context):
     scheduled_event = False
     sns_event = False
     
+    # This lambda function will serve 3 kinds of requests:
+    # cf_request - Request from cloud formation to attach/delete elastic ip.
+    # scheduled_event - Request to update AMI(delete old, create new) every 12 hours.
+    # sns_event - Request from sns to attach elastic ip to new instance created after controller failover.
     try:
         cf_request = event.get("StackId", None)
     except:
@@ -36,22 +40,33 @@ def lambda_handler(event, context):
         pass
 
     client = boto3.client('ec2')
-    controller_instanceobj = client.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']},
+    try:
+        controller_instanceobj = client.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']},
                         {'Name': 'tag:Name','Values': [INSTANCE_TAG]}])['Reservations'][0]['Instances'][0]
-
+    except:
+        # While deleting cloud formation template, this lambda function will be called
+        # to delete AssignEIP resource. If controller instance is not present then cloud formation will be stuck in deletion.
+        # So just pass in that case.
+        if cf_request and event.get("RequestType",None) == 'Delete':
+            pass
+        else:
+            return
     if cf_request:
         responseStatus = 'SUCCESS'
         responseData = {}
         if event['RequestType'] == 'Create':
             lambda_client = boto3.client('lambda')
+
+            # Add autoscaling group name to lambda environment, will be used later.
             lambda_client.update_function_configuration(FunctionName=context.function_name,
                                                     Environment={'Variables': {'EIP': os.environ.get('EIP'),
                                                     'ASG_NAME': event['ResourceProperties'].get("ASG_NAME")
                                                     }})
             assign_eip(client, controller_instanceobj)
+        #Return SUCCESS in both Create and Delete cases.
         sendResponse(event, context, "SUCCESS", responseData)
-    elif scheduled_event:        
-        duplicate_ami(client, controller_instanceobj, context)
+    elif scheduled_event:
+        update_ami(client, controller_instanceobj, context)
     elif sns_event:
         assign_eip(client, controller_instanceobj)
 
@@ -62,43 +77,75 @@ def assign_eip(client, controller_instanceobj):
                                      InstanceId=controller_instanceobj['InstanceId'])
     print("Assigned elastic IP")
 
-def duplicate_ami(client, controller_instanceobj, context):
-    delete_image_n_snapshot(client)
+# For incremental snapshotting, delete older(current) image, but keep the snapshot.
+# Now create a new image from instance, which will use already present snapshot for incremental snaphotting.
+# This will be faster. Once new image is created, we can delete older snapshot.
+def update_ami(client, controller_instanceobj, context):
+    old_ami = delete_old_image(client)
     new_ami = register_new_image(client, controller_instanceobj, context)
-    update_autoscaling_conf(new_ami, controller_instanceobj, context)
+    if new_ami:
+        # Delete older snapshot only when new ami has been created. Otherwise keep it just in case.
+        delete_old_snapshot(client, old_ami)
+        update_autoscaling_conf(new_ami, controller_instanceobj, context)
 
-def delete_image_n_snapshot(client):
+def delete_old_image(client):
     try:
-        old_image = client.describe_images(
+        old_ami = client.describe_images(
             Filters=[{'Name': 'name','Values': [AMI_NAME]}],Owners=['self'])['Images'][0]['ImageId']
     except:
         print("No older backup AMI found")
         return
-    client.deregister_image(ImageId=old_image)
-    for snapshot in client.describe_snapshots(OwnerIds=['self'])['Snapshots']:
-        if re.match(r".*for ({0}) from.*".format(old_image), snapshot['Description']):
-            client.delete_snapshot(SnapshotId=snapshot['SnapshotId'])
-    print("Deleted older image and snapshot.")
+    client.deregister_image(ImageId=old_ami)
+    print("Deleted old image.")
+    return old_ami
 
+def delete_old_snapshot(client, old_ami):
+    if old_ami is None:
+        return
+    snap_id = get_snapshot_from_ami(client, old_ami)
+    if snap_id:
+        client.delete_snapshot(SnapshotId=snap_id)
+        print("Deleted old snapshot.")
+
+def get_snapshot_from_ami(client, ami_id):
+    for snapshot in client.describe_snapshots(OwnerIds=['self'])['Snapshots']:
+        if re.match(r".*for ({0}) from.*".format(ami_id), snapshot['Description']):
+            return snapshot['SnapshotId']
 
 def register_new_image(client, controller_instanceobj, context):
-    # Register new AMI from Aviatrix controller instance.
     if controller_instanceobj['NetworkInterfaces'][0]['Association']['IpOwnerId'] != 'amazon':
         EIP = controller_instanceobj['NetworkInterfaces'][0]['Association']['PublicIp']
         lambda_client = boto3.client('lambda')
         lambda_client.update_function_configuration(FunctionName=context.function_name,
                                                     Environment={'Variables': {'EIP': EIP, 
                                                     'ASG_NAME': os.environ.get('ASG_NAME'),
-                                                    'SUBNETLIST': os.environ.get('SUBNETLIST')
+                                                    'SUBNETLIST': os.environ.get('SUBNETLIST',"")
                                                     }})
-    image = client.create_image(
+    ssm_client = boto3.client('ssm')
+
+    # Send SSM message to controller instance, which will freeze controller and create new ami from controller instance.
+    ssm_cmd = ssm_client.send_command(InstanceIds=[controller_instanceobj['InstanceId']],
+                            DocumentName='AWS-RunShellScript',
+                            Comment='Demo run Aviatrix HA3',
+                            TimeoutSeconds=30,
+                            Parameters={"commands":["mongolock=`mongo --eval \"db.fsyncLock()\"`","sync","for target in $(findmnt -nlo TARGET -t ext4); do fsfreeze -f $target; done","instance=`curl -s http://169.254.169.254/latest/meta-data/instance-id`","region=`curl -s 169.254.169.254/latest/meta-data/placement/availability-zone`","region=${region%?}","aws ec2 create-image --instance-id $instance --description \"Aviatrix controller backup AMI created from active ec2 instance\" --name \"AviatrixController\" --region $region --no-reboot","for target in $(findmnt -nlo TARGET -t ext4); do fsfreeze -u $target; done","mongounlock=`mongo --eval \"printjson(db.fsyncUnlock())\"`"]})
+
+    ssm_cmdid = ssm_cmd.get('Command').get('CommandId')
+    print(ssm_cmdid)
+    time.sleep(10)
+    ssm_response = ssm_client.list_command_invocations(
+        CommandId=ssm_cmdid,
         InstanceId=controller_instanceobj['InstanceId'],
-        Description='Aviatrix controller backup AMI created from active ec2 instance',
-        Name=AMI_NAME,
-        NoReboot=True
-    )
-    print("Registered new image.")
-    return image['ImageId']
+        Details=True)
+
+    try:
+        response = ssm_response.get('CommandInvocations')[0].get('CommandPlugins')[0].get('Output')
+        ami_id = 'ami-'+re.search('ami-(.*)"', response).group(1)
+    except:
+        return None
+    else:
+        print("Registered new image.")
+        return ami_id
 
 def update_autoscaling_conf(new_ami, controller_instanceobj, context):
     ASG_NAME = os.environ.get('ASG_NAME')
@@ -147,6 +194,7 @@ def create_autoscaling_conf(asg_client, controller_instanceobj, newLaunchConfigN
                     TopicARN=sns_topic_arn)
     print('Attached ASG')
 
+# Send response to cloud formation template for custom resource creation by cloud formation
 def sendResponse(event, context, response_status, reason=None, response_data=None, physical_resource_id=None):
     response_data = response_data or {}
     response_body = json.dumps(
@@ -173,4 +221,3 @@ def sendResponse(event, context, response_status, reason=None, response_data=Non
     except HTTPError as exc:
         print("Failed executing HTTP request: {}".format(exc.code))
         return False
-
