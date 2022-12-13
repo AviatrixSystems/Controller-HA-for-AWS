91bae672d26fa1a4d12233ca2fd3da25f29ac099
Arvind <21178759+arvindsree@users.noreply.github.com>
2022-12-12 19:01:18 -0800

    AVX-32997: Update controller HA push scripts

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,10 +1,12 @@
-zip -d aviatrix_ha.zip dev_flag 
-find . -name "*.py"  | grep -Ev "./test/test.py|./push_to_s3.py" |  zip aviatrix_ha.zip -@
+zip -d ../bin/aviatrix_ha.zip dev_flag
+cd ../src
+find -name "*.py"  | grep -Ev "./test/test.py" | zip ../bin/aviatrix_ha.zip -@
+cd -
 
 if [[ "$1" == "--dev" ]]; then
     echo "Adding dev_flag" 
     touch dev_flag
-    zip aviatrix_ha.zip dev_flag 
+    zip ../bin/aviatrix_ha.zip dev_flag 
 else
     echo "Not adding dev_flag"
 fi
