469cbd8646e91a0650a724a3c292671cda17c42d
Randolph Chung <rchung@aviatrix.com>
2024-03-18 07:22:27 -0700

    Refactor scripts/push_to_s3.py and update Makefile

--- Makefile
+++ Makefile
@@ -1,7 +1,30 @@
-bin/aviatrix_ha.zip:
+SRC_FILES = $(shell find aviatrix_ha -name "*.py")
+
+bin/aviatrix_ha.zip: $(SRC_FILES) pyproject.toml poetry.lock
 	@rm -rf $@ .venv-lambda
 	poetry bundle venv --only=main .venv-lambda
 	(cd .venv-lambda/lib/python*/site-packages/ && zip -r ../../../../$@ . -x '*.pyc' -x '__pycache__' -x '*.so')
+
+bin/aviatrix_ha_dev.zip: $(SRC_FILES) pyproject.toml poetry.lock
+	@rm -rf $@ .venv-lambda
+	poetry bundle venv --only=main .venv-lambda
+	(cd .venv-lambda/lib/python*/site-packages/ && touch dev_flag && zip -r ../../../../$@ . -x '*.pyc' -x '__pycache__' -x '*.so')
+
+cft/aviatrix-aws-existing-controller-ha-dev.json: cft/aviatrix-aws-existing-controller-ha.json
+	sed 's/aviatrix_ha.zip/aviatrix_ha_dev.zip/' $< > $@
+
+.PHONY: push
+push: bin/aviatrix_ha.zip
+	poetry run python3 scripts/push_to_s3.py --dev --lambda_zip_file=bin/aviatrix_ha.zip --cft_file=cft/aviatrix-aws-existing-controller-ha.json
+
+.PHONY: push_dev
+push_dev: bin/aviatrix_ha_dev.zip cft/aviatrix-aws-existing-controller-ha-dev.json
+	poetry run python3 scripts/push_to_s3.py --dev --lambda_zip_file=bin/aviatrix_ha_dev.zip --cft_file=cft/aviatrix-aws-existing-controller-ha-dev.json
+
+.PHONY: clean
+clean:
+	rm -rf bin/*.zip
+	rm -f cft/aviatrix-aws-existing-controller-ha-dev.json
 
 .PHONY: test
 test:
--- scripts/push_to_s3.py
+++ scripts/push_to_s3.py
@@ -1,93 +1,104 @@
 """ Push script for lambda files
 use as ACCESS_KEY=xxxx SECRET_KEY=yyyy python3 push_to_s3.py
 """
-from __future__ import print_function
+import argparse
 import os
 import sys
 import traceback
 import threading
+import zipfile
+
 import requests
 import boto3
-import zipfile
 from botocore.exceptions import ClientError
 
-sys.path.append("../src")
-from api.external.ami import DEV_FLAG
+from aviatrix_ha.common.constants import DEV_FLAG
 
 try:
-    ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
-    SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    SESSION_TOKEN = os.environ.get('AWS_SESSION_TOKEN')
+    ACCESS_KEY = os.environ["AWS_ACCESS_KEY_ID"]
+    SECRET_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+    SESSION_TOKEN = os.environ.get("AWS_SESSION_TOKEN")
 except KeyError as err:
-    raise Exception("use as AWS_ACCESS_KEY_ID=xxxx AWS_SECRET_ACCESS_KEY=yyyy AWS_SESSION_TOKEN=zzz"
-                    " python push_to_s3.py. For dev add --dev") from err
+    sys.exit("Make sure AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are set")
 
 BUCKET_PREFIX = "aviatrix-lambda-"
-LAMBDA_ZIP_FILE = '../bin/aviatrix_ha.zip'
-LAMBDA_ZIP_DEV_FILE_STR = 'aviatrix_ha_dev.zip'
+LAMBDA_ZIP_FILE = "bin/aviatrix_ha.zip"
+LAMBDA_ZIP_DEV_FILE_STR = "aviatrix_ha_dev.zip"
 
 CFT_BUCKET_NAME = "aviatrix-cloudformation-templates"
 CFT_BUCKET_REGION = "us-west-2"
-CFT_FILE_NAME = "../cft/aviatrix-aws-existing-controller-ha.json"
-CFT_DEV_FILE_NAME = "aviatrix-aws-existing-controller-ha-dev.json"
+CFT_FILE_NAME = "cft/aviatrix-aws-existing-controller-ha.json"
 
 
-def push_cft_s3():
-    """ Push CFT to S3"""
-    print(" Pushing CFT")
-    s3_ = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY,
-                       region_name=CFT_BUCKET_REGION, aws_session_token=SESSION_TOKEN)
-    dst_file = os.path.basename(CFT_FILE_NAME)
-    dev = False
-    try:
-        if sys.argv[1] == "--dev":
-            print("Pushing CFT to dev bucket")
-            dev = True
-            dst_file = CFT_DEV_FILE_NAME
-            with open(CFT_FILE_NAME) as fileh:
-                if LAMBDA_ZIP_DEV_FILE_STR not in fileh.read():
-                    raise Exception(LAMBDA_ZIP_DEV_FILE_STR + " not found in lambda in CFT")
-            with zipfile.ZipFile(LAMBDA_ZIP_FILE, 'r', zipfile.ZIP_DEFLATED) \
-                    as zip_file:
-                if DEV_FLAG not in zip_file.namelist():
-                    raise Exception(f"Please add the dev flag file {DEV_FLAG} "
-                                    f"in {LAMBDA_ZIP_FILE}")
-
-    except IndexError:
-        pass
-    if not dev:
-        with open(CFT_FILE_NAME) as fileh:
+def _validate_inputs(args: argparse.Namespace):
+    if args.dev:
+        print("Pushing CFT to dev bucket")
+        with open(args.cft_file, "r", encoding="utf-8") as fileh:
+            if LAMBDA_ZIP_DEV_FILE_STR not in fileh.read():
+                raise Exception(f"{LAMBDA_ZIP_DEV_FILE_STR} not found in lambda in CFT"
+                )
+        with zipfile.ZipFile(
+            args.lambda_zip_file, "r", zipfile.ZIP_DEFLATED
+        ) as zip_file:
+            if DEV_FLAG not in zip_file.namelist():
+                raise Exception(
+                    f"Please add the dev flag file {DEV_FLAG} in {args.lambda_zip_file}"
+                )
+    else:
+        with open(args.cft_file, "r", encoding="utf-8") as fileh:
             if LAMBDA_ZIP_DEV_FILE_STR in fileh.read():
-                raise Exception(LAMBDA_ZIP_DEV_FILE_STR + " found in lambda in CFT. Not pushing")
-        with zipfile.ZipFile(LAMBDA_ZIP_FILE, 'r', zipfile.ZIP_DEFLATED) \
-                as zip_file:
+                raise Exception(
+                    f"{LAMBDA_ZIP_DEV_FILE_STR} found in lambda in CFT. Not pushing"
+                )
+        with zipfile.ZipFile(args.lambda_zip_file, "r", zipfile.ZIP_DEFLATED) as zip_file:
             if DEV_FLAG in zip_file.namelist():
-                raise Exception(f"Please remove the dev flag file {DEV_FLAG} "
-                                f"in {LAMBDA_ZIP_FILE}")
+                raise Exception(
+                    f"Please remove the dev flag file {DEV_FLAG} in {args.lambda_zip_file}"
+                )
 
+
+
+def push_cft_s3(args: argparse.Namespace):
+    """Push CFT to S3"""
+    print(" Pushing CFT")
+    _validate_inputs(args)
+    s3_ = boto3.client(
+        "s3",
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=CFT_BUCKET_REGION,
+        aws_session_token=SESSION_TOKEN,
+    )
+    dst_file = os.path.basename(args.cft_file)
     try:
-        s3_.upload_file(CFT_FILE_NAME, CFT_BUCKET_NAME, dst_file,
-                        ExtraArgs={'ACL': 'public-read'})
+        s3_.upload_file(
+            args.cft_file, CFT_BUCKET_NAME, dst_file, ExtraArgs={"ACL": "public-read"}
+        )
     except ClientError:
         print(traceback.format_exc())
 
     # Validate file push
-    url = f'https://{CFT_BUCKET_NAME}.s3.amazonaws.com/{dst_file}'
+    url = f"https://{CFT_BUCKET_NAME}.s3.amazonaws.com/{dst_file}"
     try:
-        requests.get(url)
-    except Exception:
+        requests.get(url, timeout=60)
+    except requests.RequestException:
         print("Validation failed for CFT")
     print("Pushed CFT")
 
 
-def push_lambda_file_s3():
-    """ Push lambda file to each region"""
-    ec2_ = boto3.client('ec2', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY,
-                        region_name='us-west-1', aws_session_token=SESSION_TOKEN)
-    regions = [reg['RegionName'] for reg in ec2_.describe_regions()['Regions']]
+def push_lambda_file_s3(args: argparse.Namespace):
+    """Push lambda file to each region"""
+    ec2_ = boto3.client(
+        "ec2",
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name="us-west-1",
+        aws_session_token=SESSION_TOKEN,
+    )
+    regions = [reg["RegionName"] for reg in ec2_.describe_regions()["Regions"]]
 
     threads = [
-        threading.Thread(target=push_lambda_file_in_region, args=[region])
+        threading.Thread(target=push_lambda_file_in_region, args=[args, region])
         for region in regions
     ]
     for thread in threads:
@@ -98,11 +109,16 @@ def push_lambda_file_s3():
         thread.join()
 
 
-def push_lambda_file_in_region(region):
-    """ Push"""
+def push_lambda_file_in_region(args: argparse.Namespace, region: str):
+    """Push"""
     bucket_name = BUCKET_PREFIX + region
-    s3_ = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY,
-                       region_name=region, aws_session_token=SESSION_TOKEN)
+    s3_ = boto3.client(
+        "s3",
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+        region_name=region,
+        aws_session_token=SESSION_TOKEN,
+    )
     print(f"Pushing lambda in {region}")
 
     # # Buckets are already created now
@@ -131,28 +147,40 @@ def push_lambda_file_in_region(region):
     # print(resp['PublicAccessBlockConfiguration']) for get
     # return
 
-    dst_file = os.path.basename(LAMBDA_ZIP_FILE)
-    try:
-        if sys.argv[1] == "--dev":
-            print("Pushing to dev bucket")
-            dst_file = LAMBDA_ZIP_DEV_FILE_STR
-    except IndexError:
-        pass
+    dst_file = os.path.basename(args.lambda_zip_file)
 
     try:
-        s3_.upload_file(LAMBDA_ZIP_FILE, bucket_name, dst_file, ExtraArgs={'ACL': 'public-read'})
+        s3_.upload_file(
+            args.lambda_zip_file, bucket_name, dst_file, ExtraArgs={"ACL": "public-read"}
+        )
     except ClientError:
         print(traceback.format_exc())
 
     # Validate file push
-    url = f'https://{bucket_name}.s3.amazonaws.com/{dst_file}'
+    url = f"https://{bucket_name}.s3.amazonaws.com/{dst_file}"
     try:
-        requests.get(url)
-    except Exception:
-        print("Lambda zip validation failed for %s" % region)
-    print("pushed successfully to " + region)
+        requests.get(url, timeout=60)
+    except requests.RequestException:
+        print(f"Lambda zip validation failed for {region}")
+        return
+    print(f"pushed successfully to {region}")
 
 
-if __name__ == '__main__':
-    push_cft_s3()
-    push_lambda_file_s3()
+def main():
+    """Copy CFT and Lambda files to S3"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--lambda_zip_file", default=LAMBDA_ZIP_FILE, help="Lambda zip file path"
+    )
+    parser.add_argument(
+        "--cft_file", default=CFT_FILE_NAME, help="CFT template file path"
+    )
+    parser.add_argument("--dev", action="store_true")
+    args = parser.parse_args()
+
+    push_cft_s3(args)
+    push_lambda_file_s3(args)
+
+
+if __name__ == "__main__":
+    main()
