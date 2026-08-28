2b1f93f8fc0ff72ca2a2df50de2767489856663d
IAMDAVID0921 <dguo@aviatrix.com>
2026-08-28 01:01:49 +0000

    cleanup constants

--- aviatrix_ha/common/constants.py
+++ aviatrix_ha/common/constants.py
@@ -1,6 +1,5 @@
-HANDLE_HA_TIMEOUT = 840  # 14 min
 WAIT_DELAY = 30
-INITIAL_SETUP_DELAY = 10
-API_TIMEOUT = 30
 DEV_FLAG = "dev_flag"
 TEMP_ACCOUNT_NAME = "tempacc"
+MAX_HA_RETRIES = 3  # extend lambda 15 min window 3 times, 45 min total
+LOGIN_TIME_RESERVE = 180  # 3 min reserved for post-login steps
