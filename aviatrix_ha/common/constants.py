09169efd3995762d1a3cb189e728efafd97298ce
IAMDAVID0921 <dguo@aviatrix.com>
2026-09-01 04:48:36 +0000

    resolve comments

--- aviatrix_ha/common/constants.py
+++ aviatrix_ha/common/constants.py
@@ -1,5 +1,5 @@
 WAIT_DELAY = 30
 DEV_FLAG = "dev_flag"
 TEMP_ACCOUNT_NAME = "tempacc"
-MAX_HA_RETRIES = 3  # extend lambda 15 min window 3 times, 45 min total
+MAX_HA_RETRIES = 3  # allow up to 3 re-invocations (~60 min total)
 LOGIN_TIME_RESERVE = 180  # 3 min reserved for post-login steps
